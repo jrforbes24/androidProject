692719d98eff5599299ed65f2b239758825b8737
urock24 <jrforbes24@gmail.com>
2020-12-02 21:11:14 -0700

    updated app initial commit

--- BasicPhrases/app/src/main/java/com/example/basicphrases/MainActivity.java
+++ BasicPhrases/app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.basicphrases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void saySometing(View view) {
+        Button buttonClicked =(Button) view;
+        Log.i("Button clicked", buttonClicked.getTag().toString() );
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(buttonClicked.getTag().toString(), "raw", getPackageName()));
+        mediaPlayer.start();
     }
 }
