c02c02ee57ff4ea6f8891d91d91788b8a7628877
urock24 <jrforbes24@gmail.com>
2020-12-08 15:43:00 -0700

    figured out how to keep the list out from underneath the title bar.

--- dynamicList/app/src/main/java/com/example/dynamiclist/MainActivity.java
+++ dynamicList/app/src/main/java/com/example/dynamiclist/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -14,60 +15,32 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = new TextView(this);
+        ListView nameListView =  (ListView)findViewById(R.id.holdNames);
 
+        final ArrayList <String> myHeroes = new ArrayList<String>();
+        myHeroes.add("Ghandi");
+        myHeroes.add("Moroni");
+        myHeroes.add("Thoams S. Monson");
+        myHeroes.add("Elder Bednar");
+        myHeroes.add("Elder Holland");
+        myHeroes.add("Bishop Kirk");
+        myHeroes.add("Bishop Gordon");
 
-        ListView theListView = findViewById(R.id.nameListView);
-        theListView.addHeaderView(textView);
+        final ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myHeroes);
 
-        final ArrayList<String> familyArray = new ArrayList<String> ();
+        nameListView.setAdapter(myAdapter);
 
-        familyArray.add("John");
-        familyArray.add("Lucinda");
-        familyArray.add("JT");
-        familyArray.add("Maddie");
-        familyArray.add("Jason");
-        familyArray.add("Emma");
-        familyArray.add("Elder Forbes");
-        familyArray.add("Zeke");
-        familyArray.add("Kyra");
-        familyArray.add("Lucie");
-        familyArray.add("Sven");
-
-        final ArrayList<String> familyResponses = new ArrayList<String> ();
-        familyResponses.add("John was clicked on.");
-        familyResponses.add("Lucinda was clicked on.");
-        familyResponses.add("JT was clicked on.");
-        familyResponses.add("Maddie was clicked on.");
-        familyResponses.add("Jason was clicked on.");
-        familyResponses.add("Emma was clicked on.");
-        familyResponses.add("Elder Forbes was clicked on.");
-        familyResponses.add("Zeke was clicked on.");
-        familyResponses.add("Kyra was clicked on.");
-        familyResponses.add("Lucie was clicked on.");
-        familyResponses.add("Sven was clicked on.");
-
-
-
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, familyArray);
-
-        theListView.setAdapter(arrayAdapter);
-
-        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        nameListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.i("", familyArray.get(i-1) + "This was the int sent " + i);
+
             }
         });
 
-
+        }
     }
-}
