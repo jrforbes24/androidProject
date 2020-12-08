4869968d7ddec1348e4dc7e51e3f32deb388ce21
urock24 <jrforbes24@gmail.com>
2020-12-07 21:32:55 -0700

    added arrays and onclick lister

--- dynamicList/app/src/main/java/com/example/dynamiclist/MainActivity.java
+++ dynamicList/app/src/main/java/com/example/dynamiclist/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.dynamiclist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -11,6 +13,8 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
         ListView theListView = findViewById(R.id.nameListView);
         theListView.addHeaderView(textView);
 
-        ArrayList<String> familyArray = new ArrayList<String> ();
+        final ArrayList<String> familyArray = new ArrayList<String> ();
+
         familyArray.add("John");
         familyArray.add("Lucinda");
         familyArray.add("JT");
@@ -34,10 +39,34 @@ public class MainActivity extends AppCompatActivity {
         familyArray.add("Zeke");
         familyArray.add("Kyra");
         familyArray.add("Lucie");
+        familyArray.add("Sven");
+
+        final ArrayList<String> familyResponses = new ArrayList<String> ();
+        familyResponses.add("John was clicked on.");
+        familyResponses.add("Lucinda was clicked on.");
+        familyResponses.add("JT was clicked on.");
+        familyResponses.add("Maddie was clicked on.");
+        familyResponses.add("Jason was clicked on.");
+        familyResponses.add("Emma was clicked on.");
+        familyResponses.add("Elder Forbes was clicked on.");
+        familyResponses.add("Zeke was clicked on.");
+        familyResponses.add("Kyra was clicked on.");
+        familyResponses.add("Lucie was clicked on.");
+        familyResponses.add("Sven was clicked on.");
+
+
+
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, familyArray);
 
         theListView.setAdapter(arrayAdapter);
+
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.i("", familyArray.get(i-1) + "This was the int sent " + i);
+            }
+        });
 
 
     }
