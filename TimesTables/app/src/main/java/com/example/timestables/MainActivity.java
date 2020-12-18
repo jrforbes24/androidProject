a495ac91917e2867c066ef1afb78bd9e428e2819
urock24 <jrforbes24@gmail.com>
2020-12-17 19:56:11 -0700

    Finished the times table app.

--- TimesTables/app/src/main/java/com/example/timestables/MainActivity.java
+++ TimesTables/app/src/main/java/com/example/timestables/MainActivity.java
@@ -1,33 +1,64 @@
 package com.example.timestables;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
-
 public class MainActivity extends AppCompatActivity {
-    int[] myNumbers = {1,2,3,4,5,6,7,8,9,10};
+
+    ListView theListView;
+
+    public final void generateTimesTable(int timesTableNumber) {
+        ArrayList<String> timesTableContent = new ArrayList<String>();
+
+        for (int j = 1; j <= 100; j++) {
+            timesTableContent.add(String.valueOf(j * timesTableNumber));
+        }
+        ArrayAdapter <String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, timesTableContent);
+
+        theListView.setAdapter(arrayAdapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //    Getting the ListView
-        ListView theListView = findViewById(R.id.timesTableList);
-//        Getting the seekbar
-        SeekBar timesBar = (SeekBar) findViewById(R.id.seekBar);
-//        Setting a listener for the seekbar
+
+        //        Getting the ListView
+        theListView = findViewById(R.id.timesTableList);
+
+//        Getting the seekbar and setting it up.
+        final SeekBar timesBar = (SeekBar) findViewById(R.id.seekBar);
+        int max = 20;
+        int startingPositon = 10;
+
+        timesBar.setMax(max);
+        timesBar.setProgress(startingPositon);
+
+        generateTimesTable(startingPositon);
+
         timesBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                Log.i("Seekbar changed", Integer.toString(i));
+                int min = 1;
+                int timesTableNumber;
+
+                if ( i < min) {
+                    timesTableNumber = min;
+                    timesBar.setProgress(min);
+                } else {
+                    timesTableNumber = i;
+                }
+                Log.i( "Seekbar Value", Integer.toString(timesTableNumber));
+                generateTimesTable(timesTableNumber);
+
             }
 
             @Override
@@ -40,15 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        // List to hold the numbers 1 - 10
-        final ArrayList<Integer> timesList = new ArrayList<Integer> ();
-// actually putting the numbers in the array list
-        for(int myNumber:myNumbers){
-            timesList.add(myNumber);
-        }
-// setting the array adapter
-        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, timesList);
-        theListView.setAdapter(arrayAdapter);
+
 
     }
 }
